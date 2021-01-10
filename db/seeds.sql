0dfb4c26998d8d5227ff38f6f449d5da70bf5767
Chavelyo3 <roxette.banos@gmail.com>
2021-01-10 15:21:15 -0600

    updates sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO borgors (burger_name) VALUES ('plain burger');
-INSERT INTO borgors (burger_name) VALUES ('double bacon cheeseburger');
-INSERT INTO borgors (burger_name, devoured) VALUES ('juicy cheesy', true);
-INSERT INTO borgors (burger_name) VALUES ('jalepenopoppin burger');
-INSERT INTO borgors (burger_name, devoured) VALUES ('Mexican burger', true);
-INSERT INTO borgors (burger_name) VALUES ('L.A. dog in a bun');
+INSERT INTO burgers (burger_name) VALUES ('plain burger');
+INSERT INTO burgers (burger_name) VALUES ('double bacon cheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('juicy cheesy', true);
+INSERT INTO burgers (burger_name) VALUES ('jalepenopoppin burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mexican burger', true);
+INSERT INTO burgers (burger_name) VALUES ('L.A. dog in a bun');
