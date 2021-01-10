63f1feff548312724342823d4a77c324670e8e2a
Chavelyo3 <roxette.banos@gmail.com>
2021-01-10 17:03:15 -0600

    img update

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,60 +1,49 @@
-$(function() {
-    $(".burger-eaten").on("click", function(event) {
+$(function () {
+  $(".burger-eaten").on("click", function (event) {
     let id = $(this).data("id");
-    let newSleep = $(this).data("newsleep");
-  
-    let newSleepState = {
-        sleepy: newSleep
-      };
-  
-      // Send the PUT request.
-      $.ajax("/api/cats/" + id, {
-        type: "PUT",
-        data: newSleepState
-      }).then(
-        function() {
-          console.log("changed sleep to", newSleep);
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
-  
-    $(".create-form").on("submit", function(event) {
-      // Make sure to preventDefault on a submit event.
-      event.preventDefault();
-  
-    let newCat = {
-        name: $("#ca").val().trim(),
-        sleepy: $("[name=sleepy]:checked").val().trim()
-      };
-  
-      // Send the POST request.
-      $.ajax("/api/cats", {
-        type: "POST",
-        data: newCat
-      }).then(
-        function() {
-          console.log("created new cat");
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
-  
-    $(".delete-cat").on("click", function(event) {
-    let id = $(this).data("id");
-  
-      // Send the DELETE request.
-      $.ajax("/api/cats/" + id, {
-        type: "DELETE"
-      }).then(
-        function() {
-          console.log("deleted cat", id);
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
+    let newOrder = $(this).data("newOrder");
+
+    let newOrderState = {
+      devourded: newOrder,
+    };
+
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newOrderState,
+    }).then(function () {
+      console.log("changed burger to", newOrder);
+
+      location.reload();
     });
   });
-  
+
+  $(".create-form").on("submit", function (event) {
+    event.preventDefault();
+
+    let newBurger = {
+      burger_name: $("#ca").val().trim(),
+    };
+
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger,
+    }).then(function () {
+      console.log("new burger added");
+
+      location.reload();
+    });
+  });
+
+  $(".delete-cat").on("click", function (event) {
+    let id = $(this).data("id");
+
+    // Send the DELETE request.
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE",
+    }).then(function () {
+      console.log("deleted burger", id);
+
+      location.reload();
+    });
+  });
+});
