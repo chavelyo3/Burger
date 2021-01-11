c70dccfd7a545ff20f289a100b6f947cf4320f81
Chavelyo3 <roxette.banos@gmail.com>
2021-01-10 20:35:08 -0600

    server corrected

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 let express = require("express");
 
-let PORT = process.env.PORT ||  8000;
+let PORT = process.env.PORT ||  8080;
 
 let app = express ();
 
@@ -13,7 +13,7 @@ let exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
-let routes = require("./controllers/burger_controller.js");
+let routes = require("../controllers/burgers_controller.js");
 app.use(routes);
 
 app.listen(PORT, function(){
